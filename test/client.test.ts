8b02d8a95b5a13802c2a71509315e16e2d718806
wflixu <wflixu@88.com>
2023-11-29 21:55:23 +0800

    [feat] add config to HttpClient

--- test/client.test.ts
+++ test/client.test.ts
@@ -1,20 +1,18 @@
-
+import { lastValueFrom } from 'rxjs';
 import { HttpClient } from '../src/client';
 import { expect, test, describe, it } from 'vitest';
-
+const http = new HttpClient({
+  baseURL: 'http://127.0.0.1:8443',
+});
 describe('suite httclient', () => {
   it('serial test get', async () => {
-    const http = new HttpClient();
-    const res: any = await http
-      .get('http://127.0.0.1:8443/ping',)
-      .toPromise();
+    const res: any = await lastValueFrom(http.get('/ping'));
     expect(res.data).toMatchInlineSnapshot('"ping success !"');
   });
   it('serial test post', async () => {
-    const http = new HttpClient();
-    const res: any = await http
-      .post('http://127.0.0.1:8443/ping',{username: 'test',age: 18})
-      .toPromise();
+    const res: any = await lastValueFrom(
+      http.post('/ping', { username: 'test', age: 18 })
+    );
     expect(res.data).toMatchInlineSnapshot(`
       {
         "age": 18,
